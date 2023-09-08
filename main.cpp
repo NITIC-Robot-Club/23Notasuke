9d34d181692010a7d53e2859e4e3c94e248b4345
ctake <ctake1235@gmail.com>
2023-09-08 23:21:14 +0900

    add: can for M2006

--- main.cpp
+++ main.cpp
@@ -1,10 +1,101 @@
 #include "mbed.h"
+#include "PS3.h"
+#include <cstdint>
 
-// main() runs in its own thread in the OS
-int main()
-{
-    while (true) {
+PS3     ps3(A0, A1);
+RawCAN  can(D4, D10, 1000000);
 
+CircularBuffer<CANMessage, 32> queue;
+
+
+void getdata(void);
+
+char RcvData[8] = {0x00};
+
+// CANMessage sendmsg(0x000, TxData, 8, CANData, CANStandard);
+
+struct C610Data{
+    unsigned ID;
+    int32_t counts;
+    int32_t rpm;
+    int32_t current;
+};
+
+
+//  受信割り込み関数
+void canListen(){
+    CANMessage Rcvmsg;
+    if (can.read(Rcvmsg)){
+        queue.push(Rcvmsg);
     }
 }
 
+void datachange(unsigned ID, struct C610Data *C610, CANMessage *msg){
+    if(ID == msg->id){
+        C610->counts = uint16_t((msg->data[0] << 8) | msg->data[1]);
+        C610->rpm = int16_t((msg->data[2] << 8) | msg->data[3]);
+        C610->current = int16_t((msg->data[4] << 8) | msg->data[5]);
+        printf("%d %d %d\n",C610->counts, C610->rpm, C610->current);
+    }
+}
+
+void TorqueToBytes(uint16_t torqu, unsigned char *upper, unsigned char *lower){
+    *upper = (torqu >> 8) & 0xFF;
+    *lower = torqu & 0XFF;
+}
+
+void sendData(const int32_t torqu0, const int32_t torqu1){
+    int16_t t0,t1;
+    if(torqu0>32000){
+        t0 = 32000;
+    }else if(torqu0<-32000){
+        t0 = -32000;
+    }else{
+        t0 = torqu0;
+    }
+    if(torqu1>32000){
+        t1 = 32000;
+    }else if(torqu1<-32000){
+        t1 = -32000;
+    }else{
+        t1 = torqu1;
+    }
+
+    CANMessage msg;
+    msg.id = 0x200;
+    TorqueToBytes(t0, &msg.data[0], &msg.data[1]);
+    TorqueToBytes(t1, &msg.data[2], &msg.data[4]);
+    for(int i=5; i<8; i++){
+        msg.data[i] = 0x00;
+    }
+    can.write(msg);
+}
+
+
+bool maru,batu;
+
+int main(void){
+    can.attach(&canListen, CAN::RxIrq);
+
+    struct C610Data M1;
+    M1.ID = 0x201;
+    CANMessage Rxmsg;
+    while(true){
+        while(!queue.empty()){
+            queue.pop(Rxmsg);
+            datachange(M1.ID, &M1, &Rxmsg);
+        }
+        if(maru){
+            sendData(32000, 32000);
+        }
+        else if(batu){
+            sendData(0, 0);
+        }
+        else sendData(0, 0);
+    }
+}
+
+void getdata(void){
+    maru = ps3.getButtonState(PS3::maru);
+    batu = ps3.getButtonState(PS3::batu);
+}
